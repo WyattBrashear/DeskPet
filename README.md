7be0a620beeaebebc8212f7d65196548dbf50828
WyattBrashear <158331550+WyattBrashear@users.noreply.github.com>
2025-05-07 06:55:25 -0400

    Update files and add config

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# DeskPet
-A cute little (totally not evil) pet that lives on your desktop!
-
 # Welcome to Deskpet!
 Deskpet is a small pet that sits on your desktop!
 
 # Pet types (Edit in config.json)
     Prophet Orpheus
     Burger The Cat
-    Pizza The Dog3
+    Pizza The Dog
+    Custom Pet!
+
+# Taking care of your pet
+Your Deskpet eats files. It gets less hungry depending on the size of the file.
+
+Remember. If you don't feed it. It will start eating the files on your desktop.
+
--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import pygame
 import os
+import json
+import random
+import time
+import Cocoa
 
-window_x = 100
-window_y = 50
+#Setup
+pygame.init()
+
+def getfilesindirectory(directory):
+    files = []
+    for root, dirs, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith('.txt'):
+                files.append(os.path.join(root, filename))
+    return files
+
+def eatFile(file):
+    global fullness 
+    print("Eating file:", file)
+    if os.path.exists(file):
+        file_size = os.path.getsize(file) / (1024 * 1024) 
+        print(f"File size: {file_size} MB")
+        fullness += file_size
+        print(f"Fullness increased to: {fullness} MB")
+        os.remove(file)
+    else:
+        print("File not found!")
+
+window_x = 500
+window_y = 350
 display_x = pygame.display.Info().current_w
 display_y = pygame.display.Info().current_h
 
@@ -11,20 +38,74 @@ y = display_y - window_y
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = f"{x},{y}"
 
-pygame.init()
+try:
+    with open('config.json', 'r') as config_file:
+        config_data = json.load(config_file)
+    pet = config_data.get('pet', 'burger')
+    hthreshold = config_data.get('hunger_threshold', 5)
+    hrate = config_data.get('hunger_rate', 5)
+    emergency_food_folder = config_data.get('food_folder', 5)
+    tickspeed = config_data.get('tickspeed', 0.5)
+    if config_data.get('custom_mode', False) == 'true':
+        custom_mode = True
+        pet = config_data.get('custom_pet', 'burger')
+    else:
+        custom_mode = False
+        pet = config_data.get('pet', 'burger')
+except FileNotFoundError:
+    print("Config file not found. Using default pet.")
+    pet = 'burger'
+except json.JSONDecodeError:
+    print("Error in config file. Using default pet.")
+    pet = 'burger'
 
-windoww, windowh = 400, 300
+
+print("Deskpet Version 1.0, Pet:", pet)
+
+if pet == "prophet orpheus":
+    pet_sprites = ["./pets/orpheus/idle.png", "./pets/orpheus/idle2.png","./pets/orpheus/idle3.png", "./pets/orpheus/idle4.png", "./pets/orpheus/sleep.png", "./pets/orpheus/sleep2.png", "./pets/orpheus/sleep3.png"]
+elif pet == "burger":
+    pet_sprites = ["./pets/burger/idle.png", "./pets/burger/idle2.png", "./pets/burger/idle3.png", "./pets/burger/idle4.png", "./pets/burger/sleep.png", "./pets/burger/sleep2.png", "./pets/burger/sleep3.png"]
+elif pet == "pizza":
+    pet_sprites = ["./pets/pizza/idle.png", "./pets/pizza/idle2.png", "./pets/pizza/idle3.png", "./pets/pizza/idle4.png", "./pets/pizza/sleep.png", "./pets/pizza/sleep2.png", "./pets/pizza/sleep3.png"]
+
+elif custom_mode == True:
+    print("Loading custom pet...")
+    pet_sprites = [f"./pets/{pet}/idle.png", f"./pets/{pet}/idle2.png", f"./pets/{pet}/idle3.png", f"./pets/{pet}/idle4.png", f"./pets/{pet}/sleep.png", f"./pets/{pet}/sleep2.png", f"./pets/{pet}/sleep3.png"]
+else:
+    print("Unable to load pet sprites. Defaulting to burger.")
+    pet_sprites = ["./pets/burger/idle.png", "./pets/burger/idle2.png", "./pets/burger/idle3.png",  "./pets/burger/idle4.png", "./pets/burger/sleep.png", "./pets/burger/sleep2.png", "./pets/burger/sleep3.png"]
+
+print("Sprites loaded for:", pet, pet_sprites)
+windoww, windowh = 300, 300
 
 screen = pygame.display.set_mode((windoww, windowh), pygame.NOFRAME)
 
 pygame.display.set_caption("Deskpet")
 
+fullness = 100 #fullness level in megabytes
+
+
+sleeping = False
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((255,255,255))
+    #idle animation handling
+    sprite = pygame.image.load(pet_sprites[random.randint(0,3)]).convert_alpha()
+    screen.fill((0,0,0))
+    screen.blit(sprite, (50, 50))
+    time.sleep(tickspeed)
     pygame.display.flip()
-
+    #hunger handling
+    filepath = getfilesindirectory(emergency_food_folder)
+    fullness -= hrate
+    print("Fullness:", fullness)
+    if fullness <= hthreshold:
+        print("Hunger threshold reached. Eating file...")
+        if filepath:  # Check if filepath is not empty
+            eatFile(filepath[0])
+        else:
+            print(f"No food files found in {emergency_food_folder} to eat.")
 pygame.quit()
